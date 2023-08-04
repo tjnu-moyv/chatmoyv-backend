467d0ff4b045837f184a75266a61898df26f0f95
SurKaa <suralcarywn@qq.com>
2023-08-04 16:09:07 +0800

    Update mysql-1.0.1.sql
    结构化

--- src/main/resources/sql/mysql-1.0.1.sql
+++ src/main/resources/sql/mysql-1.0.1.sql
@@ -1,105 +1,146 @@
-use moyuchat;
-
-drop table if exists `user`;
-create table `user`
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user`
 (
-    id                   bigint auto_increment,
-    username             varchar(256) unique not null,
-    password             varchar(256)        not null,
-    nickname             varchar(256)             default 'user',
-    gender               tinyint             null default 0,
-    email                varchar(256)        null default null,
-    phone_number         varchar(64)         null default null,
-    birthday             datetime            null default null,
-    address              varchar(256)        null default null,
-    education            varchar(256)        null default null,
-    occupation           varchar(256)        null default null,
-    interests            varchar(256)        null default null,
-    bio                  varchar(256)        null default null,
-    last_login_time      datetime            null default null,
-    password_change_time datetime            null default null,
-    create_time          datetime            null default CURRENT_TIMESTAMP null comment '创建时间',
-    user_status          tinyint             null default 0 not null comment '用户状态 0 - 正常',
-    role                 tinyint             null default 0 comment '用户角色',
-    is_delete            tinyint             null default 0 not null comment '是否删除',
-    update_time          datetime            null default CURRENT_TIMESTAMP null on update CURRENT_TIMESTAMP comment '更新时间',
-    primary key (id)
-) auto_increment = 1000
-  CHARACTER SET = UTF8MB4;
+    `id`                   bigint                                                        NOT NULL AUTO_INCREMENT,
+    `username`             varchar(256) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
+    `password`             varchar(256) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
+    `nickname`             varchar(256) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL     DEFAULT 'user',
+    `gender`               tinyint                                                       NULL     DEFAULT 0,
+    `email`                varchar(256) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL     DEFAULT NULL,
+    `phone_number`         varchar(64) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci  NULL     DEFAULT NULL,
+    `birthday`             datetime                                                      NULL     DEFAULT NULL,
+    `address`              varchar(256) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL     DEFAULT NULL,
+    `education`            varchar(256) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL     DEFAULT NULL,
+    `occupation`           varchar(256) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL     DEFAULT NULL,
+    `interests`            varchar(256) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL     DEFAULT NULL,
+    `bio`                  varchar(256) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL     DEFAULT NULL,
+    `last_login_time`      datetime                                                      NULL     DEFAULT NULL,
+    `password_change_time` datetime                                                      NULL     DEFAULT NULL,
+    `create_time`          datetime                                                      NULL     DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    `user_status`          tinyint                                                       NOT NULL DEFAULT 0 COMMENT '用户状态 0 - 正常',
+    `role`                 tinyint                                                       NULL     DEFAULT 0 COMMENT '用户角色',
+    `is_delete`            tinyint                                                       NOT NULL DEFAULT 0 COMMENT '是否删除',
+    `update_time`          datetime                                                      NULL     DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+    PRIMARY KEY (`id`) USING BTREE,
+    UNIQUE INDEX `username` (`username` ASC) USING BTREE
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 1000
+  CHARACTER SET = utf8mb3
+  COLLATE = utf8mb3_general_ci
+  ROW_FORMAT = Dynamic;
 
-drop table if exists `user_relations`;
-create table `user_relations`
+-- ----------------------------
+-- Table structure for user_relations
+-- ----------------------------
+DROP TABLE IF EXISTS `user_relations`;
+CREATE TABLE `user_relations`
 (
-    id          bigint auto_increment,
-    user_id     bigint   not null,
-    friend_id   bigint   not null,
-    status      tinyint       default 0,
-    create_time datetime null default CURRENT_TIMESTAMP null comment '创建时间',
-    is_delete   tinyint  null default 0 not null comment '是否删除',
-    update_time datetime null default CURRENT_TIMESTAMP null on update CURRENT_TIMESTAMP comment '更新时间',
-    primary key (id)
-) character set = UTF8MB4;
+    `id`          bigint   NOT NULL AUTO_INCREMENT,
+    `user_id`     bigint   NOT NULL,
+    `friend_id`   bigint   NOT NULL,
+    `status`      tinyint  NULL     DEFAULT 0,
+    `create_time` datetime NULL     DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    `is_delete`   tinyint  NOT NULL DEFAULT 0 COMMENT '是否删除',
+    `update_time` datetime NULL     DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+    PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB
+  CHARACTER SET = utf8mb3
+  COLLATE = utf8mb3_general_ci
+  ROW_FORMAT = Dynamic;
 
-drop table if exists `message`;
-create table `message`
+-- ----------------------------
+-- Table structure for message
+-- ----------------------------
+DROP TABLE IF EXISTS `message`;
+CREATE TABLE `message`
 (
-    id          bigint auto_increment,
-    sender_id   bigint        not null,
-    receiver_id bigint        not null,
-    type        bigint             default 0 not null,
-    content     varchar(1024) not null,
-    is_read     tinyint            default 0,
-    create_time datetime      null default CURRENT_TIMESTAMP null comment '创建时间',
-    is_delete   tinyint       null default 0 not null comment '是否删除',
-    update_time datetime      null default CURRENT_TIMESTAMP null on update CURRENT_TIMESTAMP comment '更新时间',
-    primary key (id)
-) character set = UTF8MB4;
+    `id`          bigint                                                         NOT NULL AUTO_INCREMENT,
+    `sender_id`   bigint                                                         NOT NULL,
+    `receiver_id` bigint                                                         NOT NULL,
+    `type`        bigint                                                         NOT NULL DEFAULT 0,
+    `content`     varchar(1024) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+    `is_read`     tinyint                                                        NULL     DEFAULT 0,
+    `create_time` datetime                                                       NULL     DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    `is_delete`   tinyint                                                        NOT NULL DEFAULT 0 COMMENT '是否删除',
+    `update_time` datetime                                                       NULL     DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+    PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB
+  CHARACTER SET = utf8mb4
+  COLLATE = utf8mb4_0900_ai_ci
+  ROW_FORMAT = Dynamic;
 
-drop table if exists `moment`;
-create table `moment`
+-- ----------------------------
+-- Table structure for moment
+-- ----------------------------
+DROP TABLE IF EXISTS `moment`;
+CREATE TABLE `moment`
 (
-    id            bigint auto_increment,
-    user_id       bigint        not null,
-    content       varchar(1024) not null,
-    like_count    int                default 0 not null,
-    comment_count int                default 0,
-    image_list    varchar(1024)      default null,
-    create_time   datetime      null default CURRENT_TIMESTAMP null comment '创建时间',
-    is_delete     tinyint       null default 0 not null comment '是否删除',
-    update_time   datetime      null default CURRENT_TIMESTAMP null on update CURRENT_TIMESTAMP comment '更新时间',
-    primary key (id)
-) character set = UTF8MB4;
+    `id`            bigint                                                         NOT NULL AUTO_INCREMENT,
+    `user_id`       bigint                                                         NOT NULL,
+    `content`       varchar(1024) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+    `like_count`    int                                                            NOT NULL DEFAULT 0,
+    `comment_count` int                                                            NULL     DEFAULT 0,
+    `image_list`    varchar(1024) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL     DEFAULT NULL,
+    `create_time`   datetime                                                       NULL     DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    `is_delete`     tinyint                                                        NOT NULL DEFAULT 0 COMMENT '是否删除',
+    `update_time`   datetime                                                       NULL     DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+    PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB
+  CHARACTER SET = utf8mb4
+  COLLATE = utf8mb4_0900_ai_ci
+  ROW_FORMAT = Dynamic;
 
-drop table if exists `moment_like`;
-create table `moment_like`
+-- ----------------------------
+-- Table structure for moment_comment
+-- ----------------------------
+DROP TABLE IF EXISTS `moment_comment`;
+CREATE TABLE `moment_comment`
 (
-    id          bigint auto_increment,
-    moment_id   bigint   not null,
-    user_id     bigint   not null,
-    create_time datetime null default CURRENT_TIMESTAMP null comment '创建时间',
-    is_delete   tinyint  null default 0 not null comment '是否删除',
-    update_time datetime null default CURRENT_TIMESTAMP null on update CURRENT_TIMESTAMP comment '更新时间',
-    primary key (id)
-) character set = UTF8MB4;
+    `id`          bigint                                                         NOT NULL AUTO_INCREMENT,
+    `moment_id`   bigint                                                         NOT NULL,
+    `user_id`     bigint                                                         NOT NULL,
+    `content`     varchar(1024) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+    `create_time` datetime                                                       NULL     DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    `is_delete`   tinyint                                                        NOT NULL DEFAULT 0 COMMENT '是否删除',
+    `update_time` datetime                                                       NULL     DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+    PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB
+  CHARACTER SET = utf8mb4
+  COLLATE = utf8mb4_0900_ai_ci
+  ROW_FORMAT = Dynamic;
 
-drop table if exists `moment_comment`;
-create table `moment_comment`
+-- ----------------------------
+-- Table structure for moment_like
+-- ----------------------------
+DROP TABLE IF EXISTS `moment_like`;
+CREATE TABLE `moment_like`
 (
-    id          bigint auto_increment,
-    moment_id   bigint        not null,
-    user_id     bigint        not null,
-    content     varchar(1024) not null,
-    create_time datetime      null default CURRENT_TIMESTAMP null comment '创建时间',
-    is_delete   tinyint       null default 0 not null comment '是否删除',
-    update_time datetime      null default CURRENT_TIMESTAMP null on update CURRENT_TIMESTAMP comment '更新时间',
-    primary key (id)
-) character set = UTF8MB4;
+    `id`          bigint   NOT NULL AUTO_INCREMENT,
+    `moment_id`   bigint   NOT NULL,
+    `user_id`     bigint   NOT NULL,
+    `create_time` datetime NULL     DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    `is_delete`   tinyint  NOT NULL DEFAULT 0 COMMENT '是否删除',
+    `update_time` datetime NULL     DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+    PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB
+  CHARACTER SET = utf8mb4
+  COLLATE = utf8mb4_0900_ai_ci
+  ROW_FORMAT = Dynamic;
 
-drop table if exists `favorite`;
-create table `favorite`
+-- ----------------------------
+-- Table structure for favorite
+-- ----------------------------
+DROP TABLE IF EXISTS `favorite`;
+CREATE TABLE `favorite`
 (
-    id      bigint auto_increment,
-    user_id bigint        not null,
-    content varchar(4096) not null,
-    primary key (id)
-) character set = UTF8MB4;
+    `id`      bigint                                                         NOT NULL AUTO_INCREMENT,
+    `user_id` bigint                                                         NOT NULL,
+    `content` varchar(4096) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+    PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB
+  CHARACTER SET = utf8mb4
+  COLLATE = utf8mb4_0900_ai_ci
+  ROW_FORMAT = Dynamic;
