da085e116ed5217d5fc060c806ccf43dbfed9157
SurKaa <suralcarywn@qq.com>
2023-08-05 19:12:24 +0800

    注释注解

--- src/main/java/com/example/controller/UserController.java
+++ src/main/java/com/example/controller/UserController.java
@@ -4,6 +4,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * @author 810477672
+ */
 @RestController
 @RequestMapping("/users")
 public class UserController {
